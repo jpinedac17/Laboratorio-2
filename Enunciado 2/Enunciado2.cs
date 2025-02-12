e203dda49bd149b00b07ec1ee56d8779aa2535e0
Jeiner Andy Josué Pineda Corleto <jpinedac17@miumg.edu.gt>
2025-02-11 22:06:02 -0600

    Add files via upload

---
+++ Enunciado 2/Enunciado2.cs
@@ -0,0 +1,28 @@
+﻿// Enunciado 2: Día de la semana
+
+Console.WriteLine("Ingresa un número entre el 1 al 7");
+int dia = int.Parse(Console.ReadLine());
+
+switch (dia) {
+    case 1:
+        Console.WriteLine("Lunes");
+        break;
+    case 2:
+        Console.WriteLine("Martes");
+        break;
+    case 3:
+        Console.WriteLine("Miércoles");
+        break;
+    case 4:
+        Console.WriteLine("Jueves");
+        break;
+    case 5:
+        Console.WriteLine("Viernes");
+        break;
+    case 6:
+        Console.WriteLine("Sábado");
+        break;
+    case 7:
+        Console.WriteLine("Viernes");
+        break;
+}
---
+++ Enunciado 3/Enunciado3.cs
@@ -0,0 +1,18 @@
+﻿// Enunciado 3: Verificación de acceso
+
+string usuario = "admin";
+int contraseña = 123;
+
+Console.WriteLine("Ingrese su usuario");
+if (Console.ReadLine() != usuario) {
+    Console.WriteLine("Usuario no registrado");
+    return;
+}
+
+Console.WriteLine("Ingrese su contraseña");
+if (int.Parse(Console.ReadLine()) != contraseña) {
+    Console.WriteLine("Contraseña incorrecta");
+    return;
+}
+
+Console.WriteLine("Acceso concedido");
